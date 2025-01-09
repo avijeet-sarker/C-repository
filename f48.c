4b56a6a8ca53a9f81e62684737ec64af6ab94c16
avijeet-sarker <avijeetsarker0@gmail.com>
2025-01-10 00:20:18 +0530

    f48.c

---
+++ f48.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+float love(int a , int m);
+int main ()
+{
+    float a=99;
+    float m=1;
+         printf(" Dear monisha,\n I can't express how much I love you.  It's been like 5 years but you never tried       to understand my feelings.  I've been trying to forget you but I can't. just Give me one chance.  If you give 1 percent effort, I will give 99 percent effort. Then our love will be percentage of ");
+         printf("%f", love(a,m));
+         printf("\n I love you so much. I want to spend my whole life with you.  will you marry me?");
+    return 0;
+}
+
+float love(int a ,int m){
+    float percentage= ((a+m));
+return percentage;
+}
