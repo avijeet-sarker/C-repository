4939c659dcf78ce5699a67951146b4d01584ee86
avijeet-sarker <avijeetsarker0@gmail.com>
2025-01-11 12:38:06 +0530

    f34.c

--- f34.c
+++ f34.c
@@ -4,13 +4,21 @@ void printTable(int n);
 int main()
 {
 int n ;
+int a ;
+int b ;
 printf("enter the value n:");
 scanf("%d", &n);
+printf("enter the value a:");
+scanf("%d", &a);
+printf("enter the value b:");
+scanf("%d", &b);
 printTable( n);
+printf ("%d", sum (a,b));
 return 0;
 }
 int sum( int a ,int b){
-    return a+b ;
+    int s = ((a+b));
+    return s ;
 }
 void printTable(int n){
 for(int i=1; i<=10; i++){
