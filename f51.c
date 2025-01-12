3aa3626e086af4413f0d9ca92a71809e808fa7af
avijeet-sarker <avijeetsarker0@gmail.com>
2025-01-12 15:40:07 +0530

    f51.c

--- f51.c
+++ f51.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 void square (int n);
+void _square (int *n);
 int main(){
     int n=5;
     square (n);
-    printf("%d\n", n);
-
+    printf("number=%d\n", n);
+     _square (&n);
+ printf("number=%d\n", n);
 return 0;
 
 
@@ -12,7 +14,13 @@ return 0;
 
     void square (int n){
         n = n*n;
-        printf("%d", n);
+        printf("square=%d", n);
+       
+
         
+    }
+    void _square (int *n){
+        *n = (*n) * (*n);
+        printf("square=%d",*n);
     }
 
